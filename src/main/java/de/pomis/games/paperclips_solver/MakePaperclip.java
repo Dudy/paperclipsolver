447bafeadd44b6b3cfad34dfa5f97732a3aa0e05
Dirk Podolak <dirk.podolak@gmail.com>
2018-01-05 22:05:29 +0100

    debug output removed

--- src/main/java/de/pomis/games/paperclips_solver/MakePaperclip.java
+++ src/main/java/de/pomis/games/paperclips_solver/MakePaperclip.java
@@ -1,6 +1,5 @@
 package de.pomis.games.paperclips_solver;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,13 +17,6 @@ public class MakePaperclip extends SimpleAction {
     @Override
     public void run() {
         while (running) {
-            LOG.log(Level.FINEST, "make paperclip");
-            LOG.log(Level.FINER, "make paperclip");
-            LOG.log(Level.FINE, "make paperclip");
-            LOG.log(Level.INFO, "make paperclip");
-            LOG.log(Level.WARNING, "make paperclip");
-            LOG.log(Level.SEVERE, "make paperclip");
-            LOG.log(Level.ALL, "make paperclip");
             button.click();
         }
     }
