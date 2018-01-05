34d014e6e2a06671c9c26f85cee664316c51b5de
Dirk Podolak <dirk.podolak@gmail.com>
2018-01-05 23:59:04 +0100

    first real handler of projects (first five projects only)

--- src/main/java/de/pomis/games/paperclips_solver/Projects.java
+++ src/main/java/de/pomis/games/paperclips_solver/Projects.java
@@ -1,12 +1,16 @@
 package de.pomis.games.paperclips_solver;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 public class Projects extends SimpleAction {
+
+    private static final Logger LOG = Logger.getLogger(Projects.class.getName());
 
     private static final String PREFIX = "projectButton";
     private static final int[] ORDER = new int[]{3, 42, 1, 6, 50};
@@ -26,14 +30,14 @@ public class Projects extends SimpleAction {
                 WebElement currentProject = getCurrentProject();
 
                 if (currentProject != null) {
-                    System.out.println(currentProject.isDisplayed());
-                    System.out.println(currentProject.isEnabled());
-                    System.out.println(currentProject.isSelected());
-                    System.out.println(currentProject.getCssValue("display"));
-                    System.out.println(currentProject.getAttribute("disabled"));
-                    System.out.println("------------------------------------------");
-                    currentProject.click();
+                    if (currentProject.isEnabled()) {
+                        currentProject.click();
+                        index++;
+                        LOG.log(Level.INFO, "current project finished, increased index is now {0}", index);
+                    }
                 }
+            } else {
+                LOG.log(Level.FINE, "no projects available");
             }
 
             waitASecond();
@@ -42,12 +46,15 @@ public class Projects extends SimpleAction {
 
     private WebElement getCurrentProject() {
         WebElement currentProject;
+        String id = PREFIX + ORDER[index];
 
         try {
-            currentProject = webDriver.findElement(By.id(PREFIX + ORDER[index]));
+            currentProject = webDriver.findElement(By.id(id));
         } catch (WebDriverException e) {
             currentProject = null;
         }
+        
+        LOG.log(Level.FINE, "current project (index {0}) chosen by id {1}: {2}", new Object[] { index, id, currentProject });
 
         return currentProject;
     }
